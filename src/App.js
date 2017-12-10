945807e0e49cdc1404846bdb0f01340ae3e4617c
Juan Garcia <Juan_Garcia__@hotmail.com>
2017-12-10 11:22:55 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+
+import Output from './Components/Output';
+import Form from './Components/Form';
+
+import axios  from 'axios';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleParas = this.handleParas.bind(this);
+    this.handleType = this.handleType.bind(this);
+    this.handleFormat = this.handleFormat.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+      paras: 2,
+      type: 'all-meat',
+      format: 'html',
+      text: ''
+    }
+  }
+
+  componentWillMount() {
+    this.getText();
+  }
+
+  getText() {
+    axios.get(`https://baconipsum.com/api/?type=${this.state.type}&paras=${this.state.paras}&format=${this.state.format}`)
+    .then((response) => {
+      this.setState({ text: response.data })
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
+
+  handleParas(e) {
+    this.setState({
+      paras: e.target.value
+    })
+  }
+
+  handleType(e) {
+    this.setState({
+      type: e.target.value
+    })
+  }
+
+  handleFormat(e) {
+    this.setState({
+      format: e.target.value
+    })
+  }
+
+  handleSubmit() {
+    this.getText();
+  }
+
+  render() {
+    const { paras, type, format, text } = this.state;
+    return (
+      <div className="App">
+      <h2>Bacon Ipsum Generator</h2>
+
+        <Form paras={paras}
+        type={type}
+         format={format}
+         handleParas={this.handleParas}
+         handleType={this.handleType}
+         handleFormat={this.handleFormat}
+         handleSubmit={this.handleSubmit}
+          />
+        <Output text={text}/>
+
+        <p>Made with React
+        <i className="fab fa-react fa-spin fa-2x"></i>
+        </p>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/Components/Form.js
@@ -0,0 +1,61 @@
+import React, {Component} from 'react';
+
+class Form extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleNumParas = this.handleNumParas.bind(this);
+    this.handleType = this.handleType.bind(this);
+    this.handleFormat = this.handleFormat.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleNumParas(num) {
+    this.props.handleParas(num);
+  }
+
+  handleType(type) {
+    this.props.handleType(type);
+  }
+
+  handleFormat(format) {
+    this.props.handleFormat(format);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.handleSubmit();
+  }
+
+  render() {
+    const { paras, type, format } = this.props;
+    return (
+      <form onSubmit={this.handleSubmit}>
+
+        <label htmlFor="num-paras">
+          <p># of paragraphs</p>
+          <input id="num-paras" type="number" value={paras} onChange={this.handleNumParas} />
+        </label>
+
+        <label htmlFor="type">
+          <p>type</p>
+          <select id="type" value={type} onChange={this.handleType}>
+            <option value="all-meat">all-meat</option>
+            <option value="meat-and-filler">meat-and-filler</option>
+          </select>
+        </label>
+
+        <label htmlFor="format">
+          <p>format</p>
+          <select id="format" value={format} onChange={this.handleFormat}>
+            <option value="html">html</option>
+            <option value="text">text</option>
+          </select>
+        </label>
+        <input type="submit" value="Get Bacon ipsum" />
+      </form>
+    )
+  }
+}
+
+export default Form;
